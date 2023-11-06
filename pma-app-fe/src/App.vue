b417dd1057dd05f366ecfb780f797ee4ba14fcea
Jakub Rautner <jakub.rautner@interway.sk>
2023-11-07 00:36:16 +0100

    FE - add&configure ESlint, Prettier

--- pma-app-fe/src/App.vue
+++ pma-app-fe/src/App.vue
@@ -1,17 +1,16 @@
 <script setup lang="ts">
 //import HelloWorld from './components/HelloWorld.vue'
-import Table from './components/Table.vue';
+import Properties from './components/Properties.vue'
 const items = [
-  { age: 40, first_name: "Dickerson", last_name: "Macdonald" },
-  { age: 21, first_name: "Larsen", last_name: "Shaw" },
-  { age: 89, first_name: "Geneva", last_name: "Wilson" },
-  { age: 38, first_name: "Jami", last_name: "Carney" },
-];
+    { age: 40, first_name: 'Dickerson', last_name: 'Macdonald' },
+    { age: 21, first_name: 'Larsen', last_name: 'Shaw' },
+    { age: 89, first_name: 'Geneva', last_name: 'Wilson' },
+    { age: 38, first_name: 'Jami', last_name: 'Carney' },
+]
 </script>
 
 <template>
-  <!--
-  <div>
+    <!-- <div>
     <a href="https://vitejs.dev" target="_blank">
       <img src="/vite.svg" class="logo" alt="Vite logo" />
     </a>
@@ -19,53 +18,56 @@ const items = [
       <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
   </div>
-  <HelloWorld msg="Vite + Vue" /> -->
+  <HelloWorld msg="Vite + Vue" />
   <div class="container py-4 px-3 mx-auto">
     <h1>Hello, Bootstrap and Vite!</h1>
     <button class="btn btn-primary">Primary button</button>
-  </div>
-  <table class="table table-striped">
-  <thead>
-    <tr>
-      <th scope="col">#</th>
-      <th scope="col">First</th>
-      <th scope="col">Last</th>
-      <th scope="col">Handle</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <th scope="row">1</th>
-      <td>Mark</td>
-      <td>Otto</td>
-      <td>@mdo</td>
-    </tr>
-    <tr>
-      <th scope="row">2</th>
-      <td>Jacob</td>
-      <td>Thornton</td>
-      <td>@fat</td>
-    </tr>
-    <tr>
-      <th scope="row">3</th>
-      <td colspan="2">Larry the Bird</td>
-      <td>@twitter</td>
-    </tr>
-  </tbody>
-</table>
+  </div> -->
+    <!-- <table class="table table-striped">
+    <thead>
+      <tr>
+        <th scope="col">#</th>
+        <th scope="col">First</th>
+        <th scope="col">Last</th>
+        <th scope="col">Handle</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <th scope="row">1</th>
+        <td>Mark</td>
+        <td>Otto</td>
+        <td>@mdo</td>
+      </tr>
+      <tr>
+        <th scope="row">2</th>
+        <td>Jacob</td>
+        <td>Thornton</td>
+        <td>@fat</td>
+      </tr>
+      <tr>
+        <th scope="row">3</th>
+        <td colspan="2">Larry the Bird</td>
+        <td>@twitter</td>
+      </tr>
+    </tbody>
+  </table> -->
+    <!-- <Table></Table> -->
+    <!-- <Header></Header> -->
+    <Properties></Properties>
 </template>
 
 <style scoped>
 .logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+    height: 6em;
+    padding: 1.5em;
+    will-change: filter;
+    transition: filter 300ms;
 }
 .logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
+    filter: drop-shadow(0 0 2em #646cffaa);
 }
 .logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+    filter: drop-shadow(0 0 2em #42b883aa);
 }
 </style>
